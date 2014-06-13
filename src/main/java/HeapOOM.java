62618c0229d40e7bd47db100632cd1f6c3c52d0c
aseara <aseara@163.com>
2014-06-13 09:29:21 +0800

    test in jvm advanced

---
+++ src/main/java/HeapOOM.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: aseara
+ * Date: 13-9-23
+ * Time: 下午8:09
+ */
+public class HeapOOM {
+    static class OOMObject {
+
+    }
+
+    public static void main(String[] args) {
+        List<OOMObject> list = new ArrayList<>();
+
+        while (true) {
+            list.add(new OOMObject());
+        }
+    }
+}
